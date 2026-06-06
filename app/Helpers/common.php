c342c646a60d3cd89db057308ac10abb4b9ce34e
DaviesAmedeus <amedeusedgar@gmail.com>
2026-06-06 08:04:10 +0300

    Fixing categories on home page-1

--- app/Helpers/common.php
+++ app/Helpers/common.php
@@ -110,7 +110,7 @@ if (!function_exists('navigations')) {
 if (!function_exists('tourCategories')) {
     function tourCategories()
     {
-        $categories = Category::orderBy('name', 'asc')->get();
+        $categories = Category::orderBy('name', 'asc')->limit(3)->get();
          if (!is_null($categories)) {
             return $categories;
         }
